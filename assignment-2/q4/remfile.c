3f2dbf8c4fe502a1b14c7e37965211f1e608f7d3
neerajp99 <neerajp1999@gmail.com>
2019-03-12 16:01:59 +0530

    add question in assignment 2

--- assignment-2/q4/remfile.c
+++ assignment-2/q4/remfile.c
@@ -5,17 +5,23 @@ int main(){
 	char fname[100];//Name of text file to be changed
 	char foutn[100];//Name of the output text file
 	int n;//Number to be removed from the file
+	printf("%s\n", "Enter Input file name: " );
 	fgets(fname, 100, stdin);
+	printf("%s\n", "Enter Output file name: ");
 	fgets(foutn, 100, stdin);
+	printf("%s\n", "Enter number to check and replace the word: " );
 	scanf("%d", &n);
-	printf("%d\n", n);
 
 	//Your code here
+	//Changing the input number to char to comapre it with other characters
+	char s = n + '0';
 
 	//Declaring the a pointer of file type to both input and output files
   FILE *inputFile;
 	FILE *outputFile;
-  char c;
+
+	//Initializing getWord with a size of 100 maximum characters.
+	char getWord[100];
 
 	//fgets retain the newline character, so we add one for input filename
 	fname [ strcspn(fname, "\r\n") ] = 0;
@@ -29,21 +35,33 @@ int main(){
 	//Opening the output file in write mode
 	outputFile = fopen(foutn, "w");
 
+	//Checking if the input file exists or not
 	if (inputFile == NULL) {
 		printf("%s\n", "Cannot open file");
 		exit(0);
 	}
 
-	c = fgetc(inputFile);
-	while (c != EOF)
-	{
-			printf ("%c", c);
-			c = fgetc(inputFile);
+	//Looping over and getting word by word
+	while (fscanf (inputFile, "%1023s", getWord ) == 1 ) {
+		//Initialized counter as 0 initially every time it comes inside while loop
+		int ctr = 0;
+
+		//Looping over each character of the word and increasing the counter if
+		//the number that is to be checked is not present
+		for (int i = 0 ; i < strlen(getWord) ; i++ ) {
+			if (getWord[i] != s) {
+				ctr++;
+			}
+		}
+		//if counter is less than the string length, addd the complete word to the output file
+		if (ctr == strlen(getWord) ) {
+			fprintf(outputFile, "%s ", getWord );
+		}
 	}
-
-	fclose(inputFile);
+	//Close the output file
 	fclose(outputFile);
-
+	//Close the input file
+	fclose(inputFile);
 
 	return 0;
 }
