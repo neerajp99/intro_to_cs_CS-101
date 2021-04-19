9e7da9ecb1b59bcafea530fb3910eff0fcf220c7
neerajp99 <neerajp1999@gmail.com>
2021-04-19 19:41:29 +0530

    Add problem 16

---
+++ cs1101/Neeraj_Pandey_cs1101_practice4/q16.py
@@ -0,0 +1,5 @@
+"""
+    Write a python program to find all repeated items in a set
+"""
+if __name__ == "__main__":
+    print("Sets are unique and immutable, no repeated items if a set is initialized.")
